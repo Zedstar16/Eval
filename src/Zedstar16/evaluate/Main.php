5d383777b2b60288f8c18ac5778af0b28489d982
Zed <35542555+Zedstar16@users.noreply.github.com>
2019-05-06 08:21:23 +0100

    Message when the code is run

--- src/Zedstar16/evaluate/Main.php
+++ src/Zedstar16/evaluate/Main.php
@@ -31,6 +31,7 @@ class Main extends PluginBase{
 	public function evaluate(CommandSender $sender, array $args){
         try{
             eval(implode(" ", $args));
+            $sender->sendMessage(TextFormat::AQUA . “Executing Code”);
         }catch(\ParseError $e){
             $sender->sendMessage(TextFormat::RED . "Error: " . TextFormat::RESET . $e->getMessage());
         }catch(\UnexpectedValueException $e){
